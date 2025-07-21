419f435623a477cb90f8375a0fcd18a8af35eb75
yoji.susaki <yoji-suzaki@bm-sms.co.jp>
2025-07-21 15:41:37 +0900

    整理

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     // HTML構造のルート（<html> は必ず必要）
     <html>
+      <header style={{ background: '#eee', padding: '1rem' }}>
+        <h1>My Next.js App</h1>
+      </header>
       {/* ページの本体。ここに各ページ（page.tsx）が入る */}
       <body>{children}</body>
     </html>
